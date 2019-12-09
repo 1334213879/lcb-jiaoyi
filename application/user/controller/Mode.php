54235a335cf7bde064ab208ac6ad59a34ebaf553
WingsonC <46149572+WingsonC@users.noreply.github.com>
2019-12-09 18:41:13 +0800

    129

--- application/user/controller/Mode.php
+++ application/user/controller/Mode.php
@@ -26,7 +26,7 @@ class Mode extends Common {
 		$reg['new_jk'] = db('borrow')->where('uid', $user_id)->order('id desc')->find();
 		$reg['tx'] = db('log')->where('user_id', $user_id)->where('type', 50)->select(); //提现记录
 		$reg['tb'] = db('log')->where('user_id', $user_id)->where('type', 59)->select(); //提币记录
-		$reg['user'] = db('users')->field('user_id,mention_address,nickname,is_autonym,mobile,level,nmct,token,pass,nmct_dj,xmt,money_usdt,token_address,is_red_envelope,bonus,j_bonus,yet_tx_money')->where(array('user_id' => $user_id))->find();
+		$reg['user'] = db('users')->field('user_id,autonym_img,xm,autonym_number,mention_address,nickname,is_autonym,mobile,level,nmct,token,pass,nmct_dj,xmt,money_usdt,token_address,is_red_envelope,bonus,j_bonus,yet_tx_money')->where(array('user_id' => $user_id))->find();
 		$reg['code'] = db('code')->where("`user_id`={$user_id}")->order('time desc')->select();
 		$reg['level'] = db('user_level')->where("level_id>{$reg['user']['level']}")->order('level_id desc')->select();
 		$reg['system'] = $system;
@@ -48,6 +48,13 @@ class Mode extends Common {
 			$money = 3000 - $reg['user']['yet_tx_money'];
 
 		}
+
+		$arr = explode(',', $reg['user']['autonym_img']);
+		$reg['user']['autonym_img_1'] = isset($arr[0]) ? $arr[0] : '';
+		$reg['user']['autonym_img_2'] = isset($arr[1]) ? $arr[1] : '';
+		$reg['user']['autonym_img_3'] = isset($arr[2]) ? $arr[2] : '';
+		unset($reg['user']['autonym_img']);
+			
 		$reg['end_money'] = $money;
 		return $reg;
 	}
